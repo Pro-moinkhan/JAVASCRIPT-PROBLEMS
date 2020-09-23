cce9f767396daffc787320e212e56dadb511dfb1
Pro-moinkhan <moin.siam7@gamil.com>
2020-09-23 13:51:05 +0600

    change after seeing the video

--- assignment.js
+++ assignment.js
@@ -5,28 +5,24 @@ function feetToMile(feet){
 }
 
 // woodCalculator 
-function woodCalculator(Length, Width, Thickness)/**Cubic inches */{
-    var RedWoodcalculate = (Length * Width * Thickness);
-    var BdFt1 = RedWoodcalculate / 144; /**One board feet */
-    var BlackWoodcalculate = (Length * Width * Thickness);/**Cubic feet */
-    var BdFt2 = BlackWoodcalculate / 12; /**One board feet */
-    var TotalCalcu = BdFt1 + BdFt2 ;
-    return TotalCalcu;
+function woodCalculator(table, chair, bed){
+    var total =(table * 3 + chair * 1 + bed * 5);
+    return total;
 }
-var result = woodCalculator ( 36, 24, 12,);
-console.log("total board area is:", result, "feet.");
 
 // brickCalculator 
-function brickCalculator(TotalWallArea){
-    var TotalBricksArea = 0.228 * 0.107 * 0.069; /** indian brick size (cm) */ 
-    var TotalWall_MToFeetConvert = TotalBricksArea * 0.3048;
-    var TotalBrickWall_M3ToM2Convert = TotalWallArea / 1000;
-    var claculateBricks = TotalBrickWall_M3ToM2Convert / TotalWall_MToFeetConvert ;
-    return claculateBricks;
+function brickCalculator(floor){
+    var brick = 1000;
+    if(floor > 10 && floor <= 20){
+        return (10 * 15 * brick) + ((floor - 20) * 12 * brick);
+    }
+    else if(floor > 20){
+        return (10 * 15 * brick) + (10 * 12 * brick) + ((floor - 20) * 10 * brick);
+    }
+    else{
+        return (floor * 15 * brick);
+    }
 }
-var result = brickCalculator(40);/**Cubic feet */
-var FinalResult = Math.round(result);
-console.log("total bricks number is :", FinalResult);
 
 // tinyFriends
 function firends(name){
